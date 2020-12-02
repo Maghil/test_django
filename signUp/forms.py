f9cce0ef99b2f4c8fd55076d54357b8381137572
Prasanth121 <55969402+Prasanth121@users.noreply.github.com>
2020-12-02 14:51:35 +0530

    added login page and sign up and reduced files

--- signUp/forms.py
+++ signUp/forms.py
@@ -4,6 +4,7 @@ class signUpForm(forms.Form):
     username = forms.CharField(label='user name', max_length=50)
     email = forms.CharField(label='email', max_length=50)
     password = forms.CharField(label='password', max_length=50)
+    cnfrmpassword = forms.CharField(label='cnfrmpassword', max_length=50)
 
 class loginInfo(forms.Form):
     username= forms.CharField(label='username' ,max_length=25)
--- signUp/views.py
+++ signUp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import signUpForm, loginInfo
 
 def signUp(request):
@@ -15,6 +15,7 @@ def home(request):
     if request.method == 'POST':
         if request.POST.get('username')=="maghil" and request.POST.get("password")=="maghil":
             return HttpResponse("<h1>hello "+request.POST.get('username')+"</h1><br><br><a href='../login'>LogOut</a>")
+        elif request.POST.get('username')=="" and request.POST.get("password")=="":
+            return redirect('/login')
         else:
             return HttpResponse("<h1>hello hacker </h1><br><a href='../login'>Back To Login Page</a>")
-
