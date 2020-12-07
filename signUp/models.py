eaf8461d818458150b402b8abc9289aa4923adc7
Maghil Vannan <54468795+Maghil@users.noreply.github.com>
2020-12-07 13:10:09 +0530

    1.added api application to handle apis 2.changed a little typo in signup.html 3.added models (#4)

--- signUp/models.py
+++ signUp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class user_details(models.Model ):
+    user_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=40)
+    password = models.CharField(max_length=20)
--- webapplication/settings.py
+++ webapplication/settings.py
@@ -32,13 +32,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'signUp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #added by devs
+    'rest_framework',
+    'signUp.apps.SignupConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
